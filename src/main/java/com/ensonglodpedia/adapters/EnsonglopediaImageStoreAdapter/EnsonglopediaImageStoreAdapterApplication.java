658e5e4d9e1d3289c2bbd0a8f954571869ca5c35
oliverdillon <45541944+oliverdillon@users.noreply.github.com>
2021-04-11 16:25:38 +0100

    Initial Commit

---
+++ src/main/java/com/ensonglodpedia/adapters/EnsonglopediaImageStoreAdapter/EnsonglopediaImageStoreAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.ensonglodpedia.adapters.EnsonglopediaImageStoreAdapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsonglopediaImageStoreAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnsonglopediaImageStoreAdapterApplication.class, args);
+	}
+
+}
